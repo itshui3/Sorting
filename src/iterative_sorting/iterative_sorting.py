cf644d944a0d529643011946eb4d4158ec990280
Hui <jeffrey.x.hui@gmail.com>
2020-04-13 13:28:19 -0700

    bubble_sort complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -22,16 +22,35 @@ def selection_sort( arr ):
         arr[i] = cur_smol
 
     return arr
+
+# TO-DO:  implement the Bubble Sort function below
+def bubble_sort( arr ):
+    #BubbleLogic
+    # Write an iterative loop starting at 0
+    # Ending at the second last index
+    # : because last index would have nothing to compare with anyway
+    swap_counter = 1
+    while(swap_counter):
+        swap_counter = 0
+        for i in range(0, len(arr)-1):
+            if(arr[i] > arr[i + 1]):
+                #SwapLogic
+                cur_item = arr[i]
+                swap_item = arr[i + 1]
+
+                arr[i] = swap_item
+                arr[i + 1] = cur_item
+
+                swap_counter += 1
+
+    return arr
+
 myList = []
 for i in range(0, 20):
     myList.append(random.randint(0, 20))
 print(myList)
 
-print(selection_sort(myList))
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
-
-    return arr
+print(bubble_sort(myList))
 
 
 # STRETCH: implement the Count Sort function below
