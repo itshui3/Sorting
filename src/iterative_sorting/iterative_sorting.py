a24c42619b93e6e338b4ee69646375e13cdfdee8
Hui <jeffrey.x.hui@gmail.com>
2020-04-14 11:53:13 -0700

    count sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,5 +1,10 @@
 # TO-DO: Complete the selection_sort() function below 
 import random
+myList = []
+for i in range(0, 20):
+    myList.append(random.randint(0, 20))
+print(myList, 'original list')
+
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -22,7 +27,7 @@ def selection_sort( arr ):
         arr[i] = cur_smol
 
     return arr
-
+print(selection_sort(myList), 'selection sorted list')
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
     #BubbleLogic
@@ -31,6 +36,7 @@ def bubble_sort( arr ):
     # : because last index would have nothing to compare with anyway
     swap_counter = 1
     while(swap_counter):
+        #How do I calculate this in terms of Big(O)
         swap_counter = 0
         for i in range(0, len(arr)-1):
             if(arr[i] > arr[i + 1]):
@@ -45,15 +51,48 @@ def bubble_sort( arr ):
 
     return arr
 
-myList = []
-for i in range(0, 20):
-    myList.append(random.randint(0, 20))
-print(myList)
 
-print(bubble_sort(myList))
+
+print(bubble_sort(myList), 'bubble sorted list')
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
+    k = 0
+    for i in arr:
+        if(i > k):
+            k = i
+  
+    occur = []
+    for i in range(0, k + 1):
+        occur.append(0)
 
-    return arr
+    for i in arr:
+        occur[i] += 1
+
+    accumulation = []
+    acc = 0
+
+    for i in occur:
+        acc += i
+        accumulation.append(acc)
+
+    shift = []
+    shift.append(0)
+    for i in range(0, len(accumulation) - 1):
+        shift.append(accumulation[i])
+
+    newList = []
+    for i in arr:
+        newList.append(0)
+
+    for i in arr:
+        newList[shift[i]] = i
+        shift[i] += 1
+    return newList
+
+print(count_sort(myList), 'count sorted list')
+
+#Count sort determines the number of objects with distinct values
+
+#Then we do some arithmetic to dtermine the position of each object in the output sequence (On what basis?)
